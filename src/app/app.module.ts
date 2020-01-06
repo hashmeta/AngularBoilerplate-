26296bad130ba37fea7d1c727e511c08f05425d9
ruinedmaniac <ruined.maniac@gmail.com>
2020-01-06 21:42:49 +0530

    BoilerPlate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {PostCreateComponet} from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponet
   ],
   imports: [
     BrowserModule,
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+@Component({
+    selector: 'app-postCreate',
+    templateUrl: './post-create.component.html',
+    styleUrls: ['./post-create.component.css']
+  })
+export class PostCreateComponet{
+
+}
